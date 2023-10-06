e2539c3d0d87882ec56529ea3cc64bcbf09586f0
shoaib inamdar <123710480+shoaib-inamdar@users.noreply.github.com>
2023-10-07 00:37:59 +0530

    Add files via upload

---
+++ 3dsongs.js
@@ -0,0 +1,210 @@
+var btn=document.querySelector(".btn");
+var con=document.querySelector(".container");
+var img=document.querySelector(".img");
+var ima=document.querySelector("#img");
+var play=document.querySelector("#play");
+var music=document.querySelector("audio");
+var title=document.querySelector(".title");
+var artist=document.querySelector(".artist");
+var next=document.querySelector("#next");
+var prev=document.querySelector("#pre");
+var playico=document.querySelector(".play")
+var progressbar=document.querySelector(".progressbar");
+var progress=document.querySelector(".progress");
+var currentsong=document.querySelector(".current");
+var songduration=document.querySelector(".duration");
+var main=document.querySelector(".main");
+var backimg=document.querySelector("#backimg");
+var newsong=document.querySelectorAll("li");
+var list=document.querySelector(".list");
+var category=document.querySelector(".category");
+const songs=[{
+    name:"Under-The-Influence",
+    title:"Under-The-Influence",
+    artist:"Chris Brown",
+    back:""
+},
+{
+    name:"dilbar-dilbar",
+    title:"dilbar-dilbar",
+    artist:"NEHA KAKKAR,DHVANI BHANUSALI,IKKA",
+    back:""
+},
+{
+    name:"Mercy",
+    title:"Mercy",
+    artist:"Badshah",
+    back:""
+},
+{
+    name:"Heat-Waves",
+    title:"Heat-Waves",
+    artist:"Glass Animals",
+    
+},
+];
+var flag=true
+btn.addEventListener("click",()=>{
+    if(flag){
+    con.style.flexDirection="row";
+    con.style.bottom=0;
+    con.style.height="15%"
+    con.style.width='80%';
+    img.style.width="10%"
+    img.style.height="100%"
+    ima.style.borderRadius="0%"
+    btn.style.left="5%"
+    progressbar.style.top="-10%"
+    progressbar.style.width="95%"
+    btn.style.transform="rotate(180deg)"
+    category.style.height="60%"
+    category.style.opacity=0
+    list.style.opacity=0
+    list.style.height="60%"
+    con.style.position="absolute"
+    flag=false
+}
+else{
+    con.style.flexDirection="column";
+    con.style.position="relative"
+    con.style.height="70%"
+    con.style.width='30%';
+    img.style.width="50%"
+    img.style.height="50%"
+    ima.style.borderRadius="20%"
+    progressbar.style.position="absolute"
+    progressbar.style.top="80%"
+    progressbar.style.width="80%"
+    btn.style.left="10%"
+    category.style.height="70%"
+    list.style.height="70%"
+    category.style.opacity=1
+    list.style.opacity=1
+    btn.style.transform="rotate(3600deg)"
+    flag=true
+}
+})
+var isplay=true
+play.addEventListener("click",()=>{
+    if(isplay){
+        music.play();
+        playico.src="pause.svg"
+        isplay=false
+    }
+    else{
+        music.pause();
+        playico.src="play.svg"
+        isplay=true
+    }
+})
+
+const loadSong=(songs)=>{
+    title.textContent=songs.title;
+    artist.textContent=songs.artist;
+    music.src="3d-audiosongs/"+songs.name+".m4a";
+    ima.src="3d-audioimages/"+songs.name+".jpg";
+    backimg.src="3d-audioimages/"+songs.name+".jpg";
+}
+songindex=0;
+const nextsong=()=>{
+    
+        songindex=(songindex+1)%songs.length;
+        //default index starts from 0
+        //but default index for array objects is 1
+        // if total songs= 3 i.e total index=2
+        // and song length is 3
+        //2 =(2+1)%3
+        //here 3%3=0
+        //therefore the songs get reset to 0
+        loadSong(songs[songindex]);
+        playico.src="pause.svg"
+        if (music.duration > 0 && !music.paused) {
+            playico.src="play.svg"
+        } else {
+            playico.src="pause.svg"
+        }
+        music.play();
+};
+const prevsong=()=>{
+    songindex=(songindex-1+songs.length)%songs.length;
+    loadSong(songs[songindex]);
+    if (music.duration > 0 && !music.paused) {
+        playico.src="play.svg"
+    } else {
+        playico.src="pause.svg"
+    }
+    music.play();
+}
+
+music.addEventListener("timeupdate",(event)=>{
+    const {currentTime,duration}=event.srcElement;
+     var minduration=Math.floor(duration/60);
+     var secduration=Math.floor(duration%60);
+     songduration.innerHTML=minduration+":"+secduration;
+
+     var mincurrent=Math.floor(currentTime/60);
+     var seccuurent=Math.floor(currentTime%60);
+     currentsong.innerHTML=mincurrent+":"+seccuurent;
+     if(music.ended)
+    {
+        nextsong();
+    }
+})
+next.addEventListener("click",nextsong);
+prev.addEventListener("click",prevsong);
+
+ music.ontimeupdate=function(e){
+    progress.style.width=Math.floor(music.currentTime*100/music.duration)+"%";
+ }
+ progressbar.onclick=function(e){
+   music.currentTime=((e.offsetX/progressbar.offsetWidth)*music.duration);
+}
+
+function song0(){
+    songindex=0;
+    loadSong(songs[songindex]);
+    music.play();
+    playico.src="pause.svg"
+}
+function song1(){
+    songindex=1;
+    loadSong(songs[songindex]);
+    music.play();
+    playico.src="pause.svg"
+}
+function song2(){
+    songindex=2;
+    loadSong(songs[songindex]);
+    music.play();
+    playico.src="pause.svg"
+}
+function song3(){
+    songindex=3;
+    loadSong(songs[songindex]);
+    music.play();
+    playico.src="pause.svg"
+}
+function song4(){
+    songindex=4;
+    loadSong(songs[songindex]);
+    music.play();
+    playico.src="pause.svg"
+}
+function song5(){
+    songindex=5;
+    loadSong(songs[songindex]);
+    music.play();
+    playico.src="pause.svg"
+}
+function song6(){
+    songindex=6;
+    loadSong(songs[songindex]);
+    music.play();
+    playico.src="pause.svg"
+}
+function song7(){
+    songindex=7;
+    loadSong(songs[songindex]);
+    music.play();
+    playico.src="pause.svg"
+}
---
+++ script.js
@@ -0,0 +1,157 @@
+gsap.registerPlugin(ScrollTrigger);
+const tl=gsap.timeline();
+// tl.to(".main",{x:window.innerWidth})
+// gsap.to(".main",{z
+tl.to(".loader",{
+    y:-1000,
+    duration:2,
+    delay:2
+})
+gsap.to(".loader img",{
+    rotate:"360",
+    duration:.9,
+    repeat:-1,
+    yoyo:true,
+})
+tl.from("nav,.logo ,.name, nav ul li",{
+    y:-100,
+    opacity:0,
+    stagger:.1
+})
+gsap.from(".page2 h1,.page2 .c1 h4,.page2 .c2 h4",{
+    y:200,
+    opacity:0,
+    stagger:.2,
+    scrollTrigger:{
+        trigger:".page2",
+        scroller:"body",
+        start:"top 60%"
+    }
+})
+gsap.to(".page2 .musical",{
+    // x:500,
+    y:-500,
+    rotate:-90,
+    // duration:1,
+    // scrub:1,
+    scrollTrigger:{
+        trigger:".page2",
+        scroller:"body",
+        scrub:1,
+        start:"top 50%",
+        // markers:true
+    }
+})
+gsap.from(".page2 .song",{
+    // x:500,
+    y:-300,
+    rotate:90,
+    // duration:1,
+    // scrub:1,
+    scrollTrigger:{
+        trigger:".page2",
+        scroller:"body",
+        scrub:1,
+        start:"top 50%",
+        // markers:true
+    }
+})
+gsap.to(".page3",{
+    background:"black",
+    scrollTrigger:{
+        trigger:".page3 .box",
+        scroller:"body",
+        start:"top 60%"
+        // scrub:1
+        // markers:true
+    }
+})
+// gsap.from(".page3 h1,.page3 .content,.page3 .box",{
+//     opacity:0,
+//     stagger:.2,
+//     // duration:.5,
+//     // scrub:1,
+//     scrollTrigger:{
+//         trigger:".page3",
+//         scroller:"body",
+//         pin:true
+//         // scrub:1,
+//         // marker:true
+//     }
+// })
+gsap.to(".scrolldown",{
+    y:50,
+    repeat:-1,
+    yoyo:true,
+    duration:.5
+})
+// gsap.to(".page1 .para1 h5",{
+//     x:-500,
+//     scrollTrigger:{
+//         trigger:".page1",
+//         scroller:"body",
+//         scrub:1
+//         // pin:true
+//     }
+//     // duration:1,
+//     // repeat:-1,
+//     // yoyo:true
+// })
+// gsap.to(".page1 .para2 h5",{
+//     x:500,
+//     scrollTrigger:{
+//         trigger:".page1",
+//         scroller:"body",
+//         scrub:1
+//         // pin:true
+//     }
+// })
+// gsap.to(".page1 .name span",{
+//     opacity:0,
+//     repeat:-1,
+//     stagger:.1,
+//     // yoyo:true
+// })
+// tl.from(".page2",{
+//     scale:0,
+//     opacity:0,
+//     // rotate:360,
+//     scrollTrigger:{
+//         trigger:".page2 .content",
+//         scroller:"body",
+//         start:"top 150%",
+//         scrub:1
+//     }
+// })
+// gsap.from(".page3 .box",{
+//     x:100,
+//     opacity:0,
+//     stagger:.4,
+//     // duration:.5,
+//     scrollTrigger:{
+//         trigger:".page3 .box",
+//         scroller:"body",
+//         // start:"top 60%",
+//         pin:true
+//         // marker:true
+//     }
+// })
+// tl.from(".page3 .box",{
+//     y:1000,
+//     opacity:0
+// })
+// tl.from("nav ul li",{
+//     y:-100,
+//     opacity:0,
+//     stagger:.1,
+//     // duration:.5
+// })
+// var main=document.querySelector(".main");
+// var progress=document.querySelector(".progress");
+// // main.addEventListener("onscroll",function(){
+//     // })
+//     var x=0;
+//     function scroll(){
+//             progress.style.heigth=x++;
+        
+// }
